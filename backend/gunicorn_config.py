ab5d62ae927f3b602dea2da3166f70c2f7e0b8d7
gabrielmocanu <gabi.mocanu98@gmail.com>
2020-05-05 00:28:57 +0300

    [backend] Resolve issue about export PYTHONPATH and adding to gunicorn_config.py

--- backend/gunicorn_config.py
+++ backend/gunicorn_config.py
@@ -1,5 +1,11 @@
-from source.config import Configuration
+import sys
 
+try:
+	sys.path.index('.')
+except ValueError:
+	sys.path.append('.')
+
+from source.config import Configuration
 
 def on_starting(server):
     Configuration.preinitialize(server)
