f8cffc3161368895eedbd96eceda700ed5223af2
Radu Nicolau <radunicolau102@gmail.com>
2020-05-08 15:56:23 +0300

    cracker: Fix regex to match PMKID succeful crack
    Signed-off-by: Radu Nicolau <radunicolau102@gmail.com>

--- cracker/config.py
+++ cracker/config.py
@@ -35,7 +35,7 @@ class Configuration(object):
     hashcat_potfile_path = os.path.join(attack_path, 'hashcat.pot')
 
     # Cracking regexes
-    hashcat_show_regex = re.compile("[0-9a-f]*:[0-9a-f]{12}:.*[:*](.*)[\n]?$")
+    hashcat_show_regex = re.compile("[0-9a-f]*[:*][0-9a-f]{12}[:*].*[:*](.*)[\n]?$")
     atoi_regex = re.compile(" *[-]?[0-9]*")
 
     # Cracking variables
