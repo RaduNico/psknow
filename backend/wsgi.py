7a4853cdd84ae546eb2d96daca9a240ee074da10
Radu Nicolau <radunicolau102@gmail.com>
2023-06-17 17:55:37 +0300

    backend: fix too large cookies
    Signed-off-by: Radu Nicolau <radunicolau102@gmail.com>

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -5,5 +5,6 @@ from source.config import Configuration
 application.secret_key = Configuration.get_key_from_file("keys/secret_key")
 Configuration.api_secret_key = Configuration.get_key_from_file("keys/api_secret_key")
 
+application.config['SESSION_TYPE'] = 'filesystem'
 application.config["MAX_CONTENT_LENGTH"] = 6 * 1024 * 1024
 Configuration.initialize()
